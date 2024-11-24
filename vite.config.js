356d35c7f85c2a01b499e6994501b3b38f8137e4
Pranjal Rai <Pranjal.Rai>
2024-11-24 18:35:52 +0530

    chore: proxy config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, './env');
+    return {
+        plugins: [react()],
+        envDir: './env',
+        server: {
+            proxy: {
+                "/api": {
+                    target: env.VITE_REACT_APP_BASE_URL,
+                    changeOrigin: true,
+                    headers: true,
+                    rewrite: (path) => path.replace(/^\/api/, "/api/v1")
+                },
+            },
+        },
+    };
+});
