a8f8168c8b2cd7a725bdb9108fc85512a8782a86
Pranjal Rai <pranjal.rai>
2024-12-01 18:17:06 +0530

    feat: registration

--- src/components/user-registration/UserRegistration.jsx
+++ src/components/user-registration/UserRegistration.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import "./UserRegistration.scss";
@@ -7,191 +7,165 @@ import axiosInstance from "../../services/config/axios-config.js";
 import { useMutation } from "react-query";
 
 const UserRegistration = () => {
-  const { t } = useTranslation();
-  const [email, setEmail] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [password, setPassword] = useState("");
-  const [userType, setUserType] = useState("Select");
-  const [errors, setErrors] = useState({});
+    const { t } = useTranslation();
+    const [email, setEmail] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState({});
 
-  const registerMutation = useMutation(
-    async (registerData) => {
-      const response = await axiosInstance.post(
-        "/api/v1/auth/register",
-        registerData
-      );
-      return response.data;
-    },
-    {
-      onSuccess: (data) => {
-        console.log("Registration successful", data);
-      },
-      onError: (error) => {
-        console.error("Registration failed", error);
-      },
-    }
-  );
+    const registerMutation = useMutation(
+        async (registerData) => {
+            const response = await axiosInstance.post(
+                "/api/v1/auth/register",
+                registerData
+            );
+            return response.data;
+        },
+        {
+            onSuccess: (data) => {
+                console.log("Registration successful", data);
+            },
+            onError: (error) => {
+                console.error("Registration failed", error);
+            },
+        }
+    );
 
-  const validateEmail = (email) => {
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailPattern.test(email);
-  };
+    const validateEmail = (email) => {
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailPattern.test(email);
+    };
 
-  const validatePassword = (password) => {
-    return password.length >= 8;
-  };
+    const validatePassword = (password) => {
+        return password.length >= 8;
+    };
 
-  const validateForm = () => {
-    const validationErrors = {};
+    const validateForm = () => {
+        const validationErrors = {};
 
-    if (!email) {
-      validationErrors.email = t("required");
-    } else if (!validateEmail(email)) {
-      validationErrors.email = t("invalidEmail");
-    }
+        if (!email) {
+            validationErrors.email = t("required");
+        } else if (!validateEmail(email)) {
+            validationErrors.email = t("invalidEmail");
+        }
 
-    if (!firstName) {
-      validationErrors.firstName = t("required");
-    }
+        if (!firstName) {
+            validationErrors.firstName = t("required");
+        }
 
-    if (!lastName) {
-      validationErrors.lastName = t("required");
-    }
+        if (!lastName) {
+            validationErrors.lastName = t("required");
+        }
 
-    if (!password) {
-      validationErrors.password = t("required");
-    } else if (!validatePassword(password)) {
-      validationErrors.password = t("invalidPassword");
-    }
+        if (!password) {
+            validationErrors.password = t("required");
+        } else if (!validatePassword(password)) {
+            validationErrors.password = t("invalidPassword");
+        }
+        return validationErrors;
+    };
 
-    if (userType === "Select") {
-      validationErrors.userType = t("required");
-    }
+    const registerUser = (e) => {
+        e.preventDefault();
+        const validationErrors = validateForm();
 
-    return validationErrors;
-  };
+        if (Object.keys(validationErrors).length > 0) {
+            setErrors(validationErrors);
+        } else {
+            setErrors({});
+            registerMutation.mutate({
+                email,
+                "firstname": firstName,
+                "lastname": lastName,
+                password,
+            });
+        }
+    };
 
-  const registerUser = (e) => {
-    e.preventDefault();
-    const validationErrors = validateForm();
+    return (
+        <Container
+            fluid
+            className="register-container d-flex align-items-center justify-content-center"
+        >
+            <Row>
+                <Col xs={ 12 } md={ 18 } lg={ 25 } className="register-box">
+                    <h2 className="text-center register-title" data-testid="signup-title">
+                        { t("signup") }
+                    </h2>
 
-    if (Object.keys(validationErrors).length > 0) {
-      setErrors(validationErrors);
-    } else {
-      setErrors({});
-      registerMutation.mutate({
-        email,
-        firstName,
-        lastName,
-        password,
-        userType,
-      });
-    }
-  };
+                    <Form onSubmit={ registerUser } role="form">
+                        <Form.Group className="mb-3" controlId="formEmail">
+                            <Form.Control
+                                type="email"
+                                placeholder={ t("emailAddress") }
+                                className="form-input"
+                                value={ email }
+                                onChange={ (e) => setEmail(e.target.value) }
+                                isInvalid={ !!errors.email }
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                { errors.email }
+                            </Form.Control.Feedback>
+                        </Form.Group>
 
-  return (
-    <Container
-      fluid
-      className="register-container d-flex align-items-center justify-content-center"
-    >
-      <Row>
-        <Col xs={12} md={18} lg={25} className="register-box">
-          <h2 className="text-center register-title" data-testid="signup-title">
-            {t("signup")}
-          </h2>
+                        <Form.Group className="mb-3" controlId="formFirstName">
+                            <Form.Control
+                                type="text"
+                                placeholder={ t("firstName") }
+                                className="form-input"
+                                value={ firstName }
+                                onChange={ (e) => setFirstName(e.target.value) }
+                                isInvalid={ !!errors.firstName }
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                { errors.firstName }
+                            </Form.Control.Feedback>
+                        </Form.Group>
 
-          <Form onSubmit={registerUser} role="form">
-            <Form.Group className="mb-3" controlId="formEmail">
-              <Form.Control
-                type="email"
-                placeholder={t("emailAddress")}
-                className="form-input"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                isInvalid={!!errors.email}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.email}
-              </Form.Control.Feedback>
-            </Form.Group>
+                        <Form.Group className="mb-3" controlId="formLastName">
+                            <Form.Control
+                                type="text"
+                                placeholder={ t("lastName") }
+                                className="form-input"
+                                value={ lastName }
+                                onChange={ (e) => setLastName(e.target.value) }
+                                isInvalid={ !!errors.lastName }
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                { errors.lastName }
+                            </Form.Control.Feedback>
+                        </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formFirstName">
-              <Form.Control
-                type="text"
-                placeholder={t("firstName")}
-                className="form-input"
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-                isInvalid={!!errors.firstName}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.firstName}
-              </Form.Control.Feedback>
-            </Form.Group>
+                        <Form.Group className="mb-3" controlId="formPassword">
+                            <Form.Control
+                                type="password"
+                                placeholder={ t("password") }
+                                className="form-input"
+                                value={ password }
+                                onChange={ (e) => setPassword(e.target.value) }
+                                isInvalid={ !!errors.password }
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                { errors.password }
+                            </Form.Control.Feedback>
+                        </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formLastName">
-              <Form.Control
-                type="text"
-                placeholder={t("lastName")}
-                className="form-input"
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-                isInvalid={!!errors.lastName}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.lastName}
-              </Form.Control.Feedback>
-            </Form.Group>
+                        <Button type="submit" className="register-button w-100">
+                            { t("signup") }
+                        </Button>
 
-            <Form.Group className="mb-3" controlId="formPassword">
-              <Form.Control
-                type="password"
-                placeholder={t("password")}
-                className="form-input"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                isInvalid={!!errors.password}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.password}
-              </Form.Control.Feedback>
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formUserType">
-              <Form.Select
-                className="form-input"
-                value={userType}
-                onChange={(e) => setUserType(e.target.value)}
-                isInvalid={!!errors.userType}
-              >
-                <option>{t("select")}</option>
-                <option>{t("monastic")}</option>
-                <option>{t("teacher")}</option>
-                <option>{t("student")}</option>
-                <option>{t("educated")}</option>
-                <option>{t("regularUser")}</option>
-              </Form.Select>
-              <Form.Control.Feedback type="invalid">
-                {errors.userType}
-              </Form.Control.Feedback>
-            </Form.Group>
-
-            <Button type="submit" className="register-button w-100">
-              {t("signup")}
-            </Button>
-
-            <div className="register-links text-center mt-3">
-              <span>{t("alreadyHaveAccount")} </span>
-              <Link to="/login" className="login-link">
-                {t("login")}
-              </Link>
-            </div>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
-  );
+                        <div className="register-links text-center mt-3">
+                            <span>{ t("alreadyHaveAccount") } </span>
+                            <Link to="/login" className="login-link">
+                                { t("login") }
+                            </Link>
+                        </div>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    );
 };
 
 export default UserRegistration;
