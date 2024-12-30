689fbe1de7c9c39366edaf0cd6220eda1e105e73
Pranjal Rai <pranjal.rai>
2024-12-30 12:04:56 +0530

    refactor

--- nginx/serve.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-printenv | sort
-
-envsubst '$BACKEND' < /etc/nginx/conf.d/pacha.conf.template > /etc/nginx/conf.d/default.conf
-
-cat /etc/nginx/conf.d/default.conf
-
-nginx -g "daemon off;"
