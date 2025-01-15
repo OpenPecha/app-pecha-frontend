709fccb879852403b6314ef5cc5e906a52933935
Sachin Mundassery <sachin.mundassery>
2025-01-15 22:32:13 +0530

    added basic ui for add and edit user profile

--- src/components/user-profile/UserProfile.jsx
+++ src/components/user-profile/UserProfile.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import "./UserProfile.scss";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Card } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import PechaUserProfile from "./pecha-user-profile/PechaUserProfile.jsx";
+import {useQuery} from "react-query";
+import axiosInstance from "../config/axios-config.js";
 
 function UserProfile() {
     const {user, isAuthenticated} = useAuth0();
     const {t} = useTranslation();
-
-    console.log(user);
+    const userInfo = useQuery("userInfo",async () => {
+        return await axiosInstance.get("/api/v1/users/info");
+    })
+    console.log(userInfo)
 
     function renderProfileViaSocialLogin() {
         return (
