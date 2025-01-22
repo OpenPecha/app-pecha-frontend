09dc65101a1ac125432a1d6f403a61aa48c48e2c
Sachin Mundassery <sachin.mundassery>
2025-01-22 22:32:33 +0530

    fetching auth 0 provider details

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,23 @@
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './components/i18next/i18n'
-import { BrowserRouter as Router } from "react-router-dom";
-import { Auth0ProviderWithNavigate } from "./config/Auth0ProviderWithNavigate.jsx";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { PechaAuthProvider } from "./config/AuthContext.jsx";
+import {BrowserRouter as Router} from "react-router-dom";
+import {Auth0ProviderWithNavigate} from "./config/Auth0ProviderWithNavigate.jsx";
+import {QueryClient, QueryClientProvider} from "react-query";
+import {PechaAuthProvider} from "./config/AuthContext.jsx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
-    <Router>
-        <Auth0ProviderWithNavigate>
-            <QueryClientProvider client={ queryClient }>
-                <PechaAuthProvider>
-                    <App />
-                </PechaAuthProvider>
-            </QueryClientProvider>
-        </Auth0ProviderWithNavigate>
-    </Router>
+  <Router>
+    <QueryClientProvider client={queryClient}>
+      <Auth0ProviderWithNavigate>
+        <PechaAuthProvider>
+          <App/>
+        </PechaAuthProvider>
+      </Auth0ProviderWithNavigate>
+    </QueryClientProvider>
+  </Router>
 )
