188e116e40b92d7942f25f4920a6d276392f5315
Pranjal Rai <xpran99>
2025-01-26 11:52:26 +0530

    feat: tolgee integration, font family

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './components/i18next/i18n'
 import { BrowserRouter as Router } from "react-router-dom";
 import { Auth0ProviderWithNavigate } from "./config/Auth0ProviderWithNavigate.jsx";
 import { QueryClient, QueryClientProvider } from "react-query";
